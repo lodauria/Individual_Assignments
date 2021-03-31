8be23a15d8468c244e615f7abf39e75eb61a221b
Lorenzo D'Auria <=>
2021-03-31 14:26:24 +0200

    Web page

--- my_script.js
+++ my_script.js
@@ -1,10 +1,44 @@
-function callAwsLambdaFunction() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("myDiv").innerHTML = this.responseText;
+function callAwsLambdaFunction(type) {
+
+	var mess = "{ \"type\": ";
+	mess = mess.concat(type, "}");
+    $.ajax({
+        url: "https://354b6u6uv8.execute-api.us-east-1.amazonaws.com/default/dashboard_handle",
+        type: 'POST',
+        data: mess,
+        dataType: 'json',
+        crossDomain: true,
+        contentType: 'application/json',
+        success: mess_handle,
+        error: function(e) {
+            console.log("Error receiving data:\n" + JSON.stringify(e));
         }
-    };
-    xhttp.open("GET", "https://test123.ap-south-1.amazonaws.com/dev", true);
-    xhttp.send();
+    });
 }
+
+var mess_handle = function (data) {
+	console.log("New data received");
+   	var myData = JSON.parse(JSON.stringify(data));
+
+    document.getElementById("proj_last").innerHTML += myData.proj_last;
+    document.getElementById("light_last").innerHTML += myData.light_last;
+
+    document.getElementById("proj_avg").innerHTML += myData.proj_avg;
+    document.getElementById("light_avg").innerHTML += myData.light_avg;
+
+    document.getElementById("proj_max").innerHTML += myData.proj_max;
+    document.getElementById("light_max").innerHTML += myData.light_max;
+
+    document.getElementById("proj_min").innerHTML += myData.proj_min;
+    document.getElementById("light_min").innerHTML += myData.light_min;
+
+    document.getElementById("proj_hist").innerHTML += myData.proj_hist;
+    document.getElementById("light_hist").innerHTML += myData.light_hist;
+
+    if (myData.act > 0){
+    	confirm("Actuation done!");
+    }
+
+};
+
+window.onload = callAwsLambdaFunction(0);
