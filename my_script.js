2eafc398d219a0fb059210930fc39d1fa8db83c5
Lorenzo D'Auria <=>
2021-04-02 15:55:43 +0200

    debug

--- my_script.js
+++ my_script.js
@@ -3,42 +3,44 @@ function callAwsLambdaFunction(type) {
 	var mess = "{ \"type\": ";
 	mess = mess.concat(type, "}");
     $.ajax({
-        url: "https://354b6u6uv8.execute-api.us-east-1.amazonaws.com/default/dashboard_handle",
+        url: "https://gcgt6w6uze.execute-api.us-east-1.amazonaws.com/default/merda",
         type: 'POST',
         data: mess,
+        headers: {
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*"
+    	},
         dataType: 'json',
         crossDomain: true,
         contentType: 'application/json',
-        success: mess_handle,
+        success: function(data){
+        	console.log("New data received");
+		   	var myData = JSON.parse(JSON.stringify(data));
+
+		    document.getElementById("proj_last").innerHTML = "Projector: " + myData.proj_last;
+		    document.getElementById("light_last").innerHTML = "Lights: " + myData.light_last;
+
+		    document.getElementById("proj_avg").innerHTML = "Projector avarage value: " + myData.proj_avg;
+		    document.getElementById("light_avg").innerHTML = "Light avarage value: " + myData.light_avg;
+
+		    document.getElementById("proj_max").innerHTML = "Projector max value: " + myData.proj_max;
+		    document.getElementById("light_max").innerHTML = "Light max value: " + myData.light_max;
+
+		    document.getElementById("proj_min").innerHTML = "Projector min value: " + myData.proj_min;
+		    document.getElementById("light_min").innerHTML = "Light min value: " + myData.light_min;
+
+		    document.getElementById("proj_hist").innerHTML = "Projector: " + myData.proj_hist;
+		    document.getElementById("light_hist").innerHTML = "Lights: " + myData.light_hist;
+
+    if (myData.act > 0){
+    	confirm("Actuation done!");
+    }
+        },
         error: function(e) {
             console.log("Error receiving data:\n" + JSON.stringify(e));
         }
     });
 }
-
-var mess_handle = function (data) {
-	console.log("New data received");
-   	var myData = JSON.parse(JSON.stringify(data));
-
-    document.getElementById("proj_last").innerHTML += myData.proj_last;
-    document.getElementById("light_last").innerHTML += myData.light_last;
-
-    document.getElementById("proj_avg").innerHTML += myData.proj_avg;
-    document.getElementById("light_avg").innerHTML += myData.light_avg;
-
-    document.getElementById("proj_max").innerHTML += myData.proj_max;
-    document.getElementById("light_max").innerHTML += myData.light_max;
-
-    document.getElementById("proj_min").innerHTML += myData.proj_min;
-    document.getElementById("light_min").innerHTML += myData.light_min;
-
-    document.getElementById("proj_hist").innerHTML += myData.proj_hist;
-    document.getElementById("light_hist").innerHTML += myData.light_hist;
-
-    if (myData.act > 0){
-    	confirm("Actuation done!");
-    }
-
-};
 
 window.onload = callAwsLambdaFunction(0);
